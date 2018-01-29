85a4018dac9ca479ec10d344afe2237dc27ff3a5
Greg Stimson <stimsong@yahoo.com>
2018-01-29 23:34:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -22,9 +22,9 @@ function pressIt() {
 
 function submitIt() {
   $('form').on('submit', function(e) {
-    if ($( 'input:first' ).val() === 'correct') {
+//    if ($( 'input:first' ).val() === 'correct') {
       alert('Your form is going to be submitted now.');
-    }
+//    }
   });
 }
 
